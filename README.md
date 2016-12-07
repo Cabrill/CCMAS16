b130ecb52dfedf718cb7cac0b0992e8e8f65209a
Jared Bagley <cabrill@gmail.com>
2016-12-08 00:25:42 +0200

    Initial push of documentation

--- README.md
+++
@@ -1,2 +0,0 @@
-# CCMAS16
-A group project for creatively generating MIDI files, created by students at University of Helsinki.
--- serializers.py
+++
@@ -1,5 +0,0 @@
-import pickle
-from creamas import Artifact
-
-def get_artifact_ser():
-        return Artifact, pickle.dumps, pickle.loads
